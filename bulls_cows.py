92e1217a6f26f824a51e23f0ba0500fd4442ea52
Tomáš Janata <tomas.janata@nempk.cz>
2023-07-21 10:16:46 +0200

    Bulls & Cows - first version for check

--- bulls_cows.py
+++ bulls_cows.py
@@ -7,4 +7,73 @@ discord: TomášJ#3088
 """
 
 # Variables
+separator = "-----------------------------------------------"
 
+
+print("Hi there!")
+print(separator)
+print("I've generated a random 4 digit number for you.")
+print("Let's play a bulls and cows game.")
+print(separator)
+
+import random
+
+#Function for generating secret four-digit number
+def generate_secret_number():
+    numbers = random.sample(range(1, 10), 4)
+    return "".join(map(str, numbers))
+
+#Function for counting number of bulls and cows
+def count_bulls_cows(secret, tip):
+    bulls, cows = 0, 0
+    for item in range(len(secret)):
+        if secret[item] == tip[item]:
+            bulls += 1
+        elif secret[item] in tip:
+            cows += 1
+    return bulls, cows
+
+#Check user tip - if it´s four-digit number, without duplicity, not started with zero, and contains numbers only
+def verify_tips(tip):
+    if not tip.isdigit() or len(tip) != 4 or len(set(tip)) != 4:
+        return False
+    if tip[0] == '0':
+        return False
+    return True
+
+#Get number of bulls or cows
+def print_bulls_cows(bulls, cows):
+    bulls_str = "bull" if bulls == 1 else "bulls"
+    cows_str = "cow" if cows == 1 else "cows"
+    print(f"{bulls} {bulls_str}, {cows} {cows_str}")
+
+#Counter of attempts
+def counter_attempts(secret):
+    secret_number = generate_secret_number()
+    attempts = 0
+    max_attempts = 10
+
+
+    while attempts < max_attempts:
+        tip = input("Enter a number: ")
+        print(separator)
+
+        if not verify_tips(tip):
+            print("Invalid tip! Enter four-digit number, without duplicity, not started with zero, and contains numbers only please.")
+            continue
+
+        bulls, cows = count_bulls_cows(secret, tip)
+        attempts += 1
+
+        if bulls == 4:
+            print(f"Correct, you've guessed the right number {secret}!")
+            break
+        else:
+                print_bulls_cows(bulls, cows)
+
+    if attempts == max_attempts:
+        print(f"Game over! Secret number was {secret}.")
+
+if __name__ == "__main__":
+    secret_number = generate_secret_number()
+    counter_attempts(secret_number)
