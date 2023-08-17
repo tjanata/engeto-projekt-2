d8b1c34a8e7eadc84fad3ce65a0a5c8a7e318950
Tomáš Janata <tomas.janata@nempk.cz>
2023-08-17 13:56:03 +0200

    Bulls & Cows - second version for check

--- bulls_cows.py
+++ bulls_cows.py
@@ -8,6 +8,7 @@ discord: TomášJ#3088
 
 # Variables
 separator = "-----------------------------------------------"
+number_of_digits = 4
 
 
 print("Hi there!")
@@ -20,8 +21,11 @@ import random
 
 #Function for generating secret four-digit number
 def generate_secret_number():
-    numbers = random.sample(range(1, 10), 4)
+    first_digit = random.choice(range(1, 10))  #first number is not zero
+    other_digits = random.sample(list(set(range(10)) - {first_digit}), 3)  #other numbers
+    numbers = [first_digit] + other_digits
     return "".join(map(str, numbers))
+
 
 #Function for counting number of bulls and cows
 def count_bulls_cows(secret, tip):
@@ -35,7 +39,7 @@ def count_bulls_cows(secret, tip):
 
 #Check user tip - if it´s four-digit number, without duplicity, not started with zero, and contains numbers only
 def verify_tips(tip):
-    if not tip.isdigit() or len(tip) != 4 or len(set(tip)) != 4:
+    if not tip.isdigit() or len(tip) != number_of_digits or len(set(tip)) != number_of_digits:
         return False
     if tip[0] == '0':
         return False
@@ -47,13 +51,13 @@ def print_bulls_cows(bulls, cows):
     cows_str = "cow" if cows == 1 else "cows"
     print(f"{bulls} {bulls_str}, {cows} {cows_str}")
 
+
 #Counter of attempts
 def counter_attempts(secret):
-    secret_number = generate_secret_number()
     attempts = 0
     max_attempts = 10
 
-
+    
     while attempts < max_attempts:
         tip = input("Enter a number: ")
         print(separator)
@@ -65,11 +69,11 @@ def counter_attempts(secret):
         bulls, cows = count_bulls_cows(secret, tip)
         attempts += 1
 
-        if bulls == 4:
+        if bulls == number_of_digits:
             print(f"Correct, you've guessed the right number {secret}!")
             break
         else:
-                print_bulls_cows(bulls, cows)
+            print_bulls_cows(bulls, cows)
 
     if attempts == max_attempts:
         print(f"Game over! Secret number was {secret}.")
